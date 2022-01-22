415455cc1a7d924b8c13008c1503bfd2bcdc8860
Anton-Zakutskyi <anton888000@gmail.com>
2022-01-22 17:55:20 +0200

    First section adaptive in progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
-    $('.header_burger').click(function(event) {
+    $('.header_burger').click(function (event) {
+        console.log(event);
         $('.header_burger,.header_menu ').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: true,
+    nav: true,
+    navText: ['', ' '],
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
